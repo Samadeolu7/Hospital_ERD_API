258168f12677442f6c7ddcaedac4713ffa1ef700
Samuel <samadeolu7@gmail.com>
2025-06-25 01:47:33 +0100

    initial commit

---
+++ src/main/java/com/tesa/hospitalerd/HospitalErdApplication.java
@@ -0,0 +1,13 @@
+package com.tesa.hospitalerd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalErdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HospitalErdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tesa/hospitalerd/HospitalErdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tesa.hospitalerd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalErdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
